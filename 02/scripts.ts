6b840792bec5eced03cb8ec10dc0014c78ff9ba4
etoregrande <etoregrande@gmail.com>
2024-10-30 10:26:57 -0300

    Exercício 02 resolvido

--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,42 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type CarrinhoComEndereco = Omit<Carrinho, `tipoTransacao`> & {
+    endereco: Endereco,
+    tipoTransacao: Lowercase<'credito' | 'Debito'>
+}
+
+//Teste do tipo
+const venda: CarrinhoComEndereco = {
+    item: {
+        nome: `Monitor`,
+        descricao: `Monitor gamer`,
+        valor: 150000
+    },
+    qtd: 1,
+    desconto: 0,
+    frete: 1000,
+    tipoTransacao: 'debito',
+    cartao: {
+        numero: 1234123412341234,
+        validade: `10/28`,
+        nome: `Etore Grande`,
+        cvv: 123
+    },
+    endereco: {
+        cep: `1231234asdfa`,
+        rua: `fdfjlsdf`,
+        bairro: `fdfsdfsd`,
+        cidade: `dfasfsdf`,
+        estado: `fdsfsdf`
+    }
+}
